9eb69039c0e388665568760d5e831241418e6901
Attila Szeremi <attila@szeremi.org>
2024-06-23 19:21:13 +0200

    Format with tabs

--- main.go
+++ main.go
@@ -1,121 +1,121 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "path/filepath"
-    "strings"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 const (
-    apiURL     = "http://example.com/api/endpoint" // Replace with your actual API endpoint
-    version    = "1.0.0"
-    mainDataDir = "main_data"
+	apiURL      = "https://raw.githubusercontent.com/amcsi/magyar-master-duel-hex-kodok/master/codes.txt"
+	version     = "1.0.0"
+	mainDataDir = "main_data"
 )
 
 func main() {
-    // Output the version number of the script
-    fmt.Printf("Script Version: %s\n", version)
-    
-    // Wait for the user to press any key to continue
-    fmt.Println("Press any key to continue...")
-    bufio.NewReader(os.Stdin).ReadBytes('\n')
-    
-    // Step 1: Call the API endpoint and get the plain text response
-    response, err := http.Get(apiURL)
-    if err != nil {
-        log.Fatalf("Error fetching API data: %v", err)
-    }
-    defer response.Body.Close()
+	// Output the version number of the script
+	fmt.Printf("Script Version: %s\n", version)
 
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        log.Fatalf("Error reading API response: %v", err)
-    }
+	// Wait for the user to press any key to continue
+	fmt.Println("Press any key to continue...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
 
-    folders := strings.Split(string(body), "\n")
-    for i := range folders {
-        folders[i] = strings.TrimSpace(folders[i])
-    }
+	// Step 1: Call the API endpoint and get the plain text response
+	response, err := http.Get(apiURL)
+	if err != nil {
+		log.Fatalf("Error fetching API data: %v", err)
+	}
+	defer response.Body.Close()
 
-    // Step 2: Check for the "main_data" folder
-    cwd, err := os.Getwd()
-    if err != nil {
-        log.Fatalf("Error getting current working directory: %v", err)
-    }
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalf("Error reading API response: %v", err)
+	}
 
-    mainDataPath := filepath.Join(cwd, mainDataDir)
-    if _, err := os.Stat(mainDataPath); os.IsNotExist(err) {
-        // Find the first folder and rename it to "main_data"
-        files, err := ioutil.ReadDir(cwd)
-        if err != nil {
-            log.Fatalf("Error reading directory: %v", err)
-        }
+	folders := strings.Split(string(body), "\n")
+	for i := range folders {
+		folders[i] = strings.TrimSpace(folders[i])
+	}
 
-        folderRenamed := false
-        for _, file := range files {
-            if file.IsDir() {
-                oldPath := filepath.Join(cwd, file.Name())
-                err = os.Rename(oldPath, mainDataPath)
-                if err != nil {
-                    log.Fatalf("Error renaming folder: %v", err)
-                }
-                folderRenamed = true
-                break
-            }
-        }
+	// Step 2: Check for the "main_data" folder
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Error getting current working directory: %v", err)
+	}
 
-        if !folderRenamed {
-            err = os.Mkdir(mainDataPath, os.ModePerm)
-            if err != nil {
-                log.Fatalf("Error creating main_data folder: %v", err)
-            }
-        }
-    }
+	mainDataPath := filepath.Join(cwd, mainDataDir)
+	if _, err := os.Stat(mainDataPath); os.IsNotExist(err) {
+		// Find the first folder and rename it to "main_data"
+		files, err := ioutil.ReadDir(cwd)
+		if err != nil {
+			log.Fatalf("Error reading directory: %v", err)
+		}
 
-    // Step 3: Create or modify folders based on the strings received
-    for _, folder := range folders {
-        if folder == "" {
-            continue
-        }
+		folderRenamed := false
+		for _, file := range files {
+			if file.IsDir() {
+				oldPath := filepath.Join(cwd, file.Name())
+				err = os.Rename(oldPath, mainDataPath)
+				if err != nil {
+					log.Fatalf("Error renaming folder: %v", err)
+				}
+				folderRenamed = true
+				break
+			}
+		}
 
-        folderPath := filepath.Join(cwd, folder)
-        linkTarget := mainDataPath
+		if !folderRenamed {
+			err = os.Mkdir(mainDataPath, os.ModePerm)
+			if err != nil {
+				log.Fatalf("Error creating main_data folder: %v", err)
+			}
+		}
+	}
 
-        if info, err := os.Lstat(folderPath); err == nil {
-            if info.Mode()&os.ModeSymlink != 0 {
-                // a) Folder is a symlink, leave it alone
-                fmt.Printf("Folder %s is a symlink, leaving it alone.\n", folder)
-                continue
-            } else {
-                // b) Folder is a regular folder, delete it and create a symlink
-                err = os.RemoveAll(folderPath)
-                if err != nil {
-                    log.Fatalf("Error removing folder %s: %v", folder, err)
-                }
-                err = os.Symlink(linkTarget, folderPath)
-                if err != nil {
-                    log.Fatalf("Error creating symlink for folder %s: %v", folder, err)
-                }
-                fmt.Printf("Folder %s was a regular folder, replaced it with a symlink.\n", folder)
-            }
-        } else if os.IsNotExist(err) {
-            // c) Folder did not exist, create a symlink
-            err = os.Symlink(linkTarget, folderPath)
-            if err != nil {
-                log.Fatalf("Error creating symlink for folder %s: %v", folder, err)
-            }
-            fmt.Printf("Folder %s did not exist, created a symlink.\n", folder)
-        } else {
-            log.Fatalf("Error checking folder %s: %v", folder, err)
-        }
-    }
+	// Step 3: Create or modify folders based on the strings received
+	for _, folder := range folders {
+		if folder == "" {
+			continue
+		}
 
-    // Step 4: Show a confirmation message before closing
-    fmt.Println("Operation completed. Press Enter to exit.")
-    fmt.Scanln()
+		folderPath := filepath.Join(cwd, folder)
+		linkTarget := mainDataPath
+
+		if info, err := os.Lstat(folderPath); err == nil {
+			if info.Mode()&os.ModeSymlink != 0 {
+				// a) Folder is a symlink, leave it alone
+				fmt.Printf("Folder %s is a symlink, leaving it alone.\n", folder)
+				continue
+			} else {
+				// b) Folder is a regular folder, delete it and create a symlink
+				err = os.RemoveAll(folderPath)
+				if err != nil {
+					log.Fatalf("Error removing folder %s: %v", folder, err)
+				}
+				err = os.Symlink(linkTarget, folderPath)
+				if err != nil {
+					log.Fatalf("Error creating symlink for folder %s: %v", folder, err)
+				}
+				fmt.Printf("Folder %s was a regular folder, replaced it with a symlink.\n", folder)
+			}
+		} else if os.IsNotExist(err) {
+			// c) Folder did not exist, create a symlink
+			err = os.Symlink(linkTarget, folderPath)
+			if err != nil {
+				log.Fatalf("Error creating symlink for folder %s: %v", folder, err)
+			}
+			fmt.Printf("Folder %s did not exist, created a symlink.\n", folder)
+		} else {
+			log.Fatalf("Error checking folder %s: %v", folder, err)
+		}
+	}
+
+	// Step 4: Show a confirmation message before closing
+	fmt.Println("Operation completed. Press Enter to exit.")
+	fmt.Scanln()
 }
